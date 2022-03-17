44c4aadedc54994984d69ee37345ba0bc61a7b28
Dillon Mears <dillonfmears@gmail.com>
2022-03-17 13:26:06 -0400

    styled code

--- profile_watch_time.py
+++ profile_watch_time.py
@@ -1,28 +1,45 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# import netflix data csv.
+# Import data and create data frame.
 df = pd.read_csv('viewing-activity-data.csv')
 
-# remove coulumns you do not want to analyze by adding them to df.drop
-df = df.drop(['Attributes', 'Supplemental Video Type', 'Device Type', 'Bookmark', 'Latest Bookmark', 'Country'], axis=1)
+# Remove unnecessary coulumns.
+df = df.drop(['Attributes', 'Supplemental Video Type', 'Device Type',
+              'Bookmark', 'Latest Bookmark', 'Country'],axis=1)
 
-# convert Duration from object to timedelta
+# Convert 'Duration' from an object to timedelta using pd.to_timedelta().
 df['Duration'] = pd.to_timedelta(df['Duration'])
 
-# group and sum profile watch time
-df_groupby_profile = df.groupby(['Profile Name'], as_index=False)['Duration'].sum()
+# Group and sum profile watch times using .groupby() and .sum().
+df_groupby_profile = df.groupby(
+    ['Profile Name'], as_index=False)['Duration'].sum()
 
-# rounds to days
+# Round 'Duration' to days using dt.days.
 df_groupby_profile['Duration'] = df_groupby_profile['Duration'].dt.days
 
-# convert Duration from timedelta to numeric for graphing
-df_groupby_profile['Duration'] = pd.to_numeric(df_groupby_profile['Duration'])
+# Convert 'Duration' from a timedelta to numeric using pd.to_numeric.
+df_groupby_profile['Duration'] = pd.to_numeric(
+    df_groupby_profile['Duration'])
 
-# create bar graph of each Profile's total Duration(watch time)
-# create bar graph of each Profile's total Duration(watch time)
-plt.bar(df_groupby_profile['Profile Name'], df_groupby_profile['Duration'], color = "#DE0912")
-plt.title('Netflix Profiles Total Watch Time', fontweight = 'bold', fontsize = 14)
-plt.xlabel('Profiles', fontweight = 'bold', fontsize = 12)
-plt.ylabel('Total Watch Time', fontweight = 'bold', fontsize = 12)
-plt.savefig('Netflix Profile Total Watch Time.png')
+# Create bar graph of each profile's summed 'Duration' using plt.bar().
+plt.bar(
+    df_groupby_profile['Profile Name'],
+    df_groupby_profile['Duration'],
+    color = "#DE0912")
+
+plt.title(
+    'Netflix Profiles Total Watch Time',
+    fontweight = 'bold',
+    fontsize = 14)
+
+plt.xlabel('Profiles',
+    fontweight = 'bold',
+    fontsize = 12)
+
+plt.ylabel(
+    'Total Watch Time (Days)',
+    fontweight = 'bold',
+    fontsize = 12)
+
+plt.show()
