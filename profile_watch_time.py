ea47a1d08d63faaaf65e3a5a715e38f94707c4f2
Dillon Mears <dillonfmears@gmail.com>
2022-03-22 21:12:38 -0400

    created days_removed to caculate days passed since data was received. added days removed to bar graph.

--- profile_watch_time.py
+++ profile_watch_time.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import datetime as dt
 
 # Import data and create data frame.
 # If you are using your own Netlix data, replace the .csv below.
@@ -23,6 +24,14 @@ df_groupby_profile['Duration'] = df_groupby_profile['Duration'].dt.days
 df_groupby_profile['Duration'] = pd.to_numeric(
     df_groupby_profile['Duration'])
 
+'''
+Retrieves the current date and calculates how many days have passed since
+the Netflix data was requested. The variable is then converted to a
+string and formatted to include only days.
+'''
+days_removed = dt.date.today() - dt.date(2022, 2, 21)
+days_removed = str(days_removed.days)
+
 # Create bar graph of each profile's summed 'Duration' using plt.bar().
 plt.bar(
     df_groupby_profile['Profile Name'],
@@ -30,7 +39,8 @@ plt.bar(
     color = "#DE0912")
 
 plt.title(
-    'Netflix Profiles Total Watch Time',
+    'Netflix Profiles Total Watch Time\n Data Requested ' + days_removed
+     + ' Days Ago',
     fontweight = 'bold',
     fontsize = 14)
 
